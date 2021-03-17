6e07a087bcaea75669752725bc90801fd8331504
Mirza <mirza.hamurko@gmail.com>
2021-03-18 00:01:57 +0100

    ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,15 @@
+// Create school object here
+var school = {
+    name: "Arena",
+    location: "Sarajevo",
+    established: "2020",
+  };
+  // End of school object
+  // Only change code below this line
+  function myFunction(name) {
+    school.name = name;
+    return school;
+  }
+  // Only change code above this line
+  myFunction("Paragon");
+  module.exports = { school, myFunction };
