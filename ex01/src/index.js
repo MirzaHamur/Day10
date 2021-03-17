b32107940b716634f889083cdeb53382fd37b12c
Mirza <mirza.hamurko@gmail.com>
2021-03-17 23:52:58 +0100

    ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "Dog",
+    name: "Mak",
+    legs: 4,
+    friends: ["Cili", "Mak"]
+};
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); 
+module.exports = { myPet, myFunction };
